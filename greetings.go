009f9fc2713ab4d0252709c26bbc29197e1ee8d8
Vitaliy Belyaev <belyaev1392@gmail.com>
2021-11-07 20:57:47 +0300

    Add Hellos function

--- greetings.go
+++ greetings.go
@@ -18,8 +18,12 @@ func Hello(name string) (string, error) {
 // Hellos returns a map that associates each of the named people
 // with a greeting message.
 func Hellos(names []string) (map[string]string, error) {
+	if len(names) == 0 {
+		return nil, errors.New("names list empty")
+	}
 	// A map to associate names with messages.
 	messages := make(map[string]string)
+
 	// Loop through the received slice of names, calling
 	// the Hello function to get a message for each name.
 	for _, name := range names {
--- greetings_test.go
+++ greetings_test.go
@@ -5,22 +5,65 @@ import (
 	"testing"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
 func TestHelloName(t *testing.T) {
+	// given
 	name := "Gladys"
 	want := regexp.MustCompile(`\b` + name + `\b`)
+	// when
 	msg, err := Hello("Gladys")
+	// then
 	if !want.MatchString(msg) || err != nil {
 		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
 	}
 }
 
-// TestHelloEmpty calls greetings.Hello with an empty string,
-// checking for an error.
 func TestHelloEmpty(t *testing.T) {
-	msg, err := Hello("")
+	// given
+	name := ""
+	// when
+	msg, err := Hello(name)
+	// then
 	if msg != "" || err == nil {
 		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
+
+func TestHellosNames(t *testing.T) {
+	// given
+	names := []string{"Gladys", "Joe", "Jack"}
+	// when
+	greetingsMap, err := Hellos(names)
+	// then
+	for name, greeting := range greetingsMap {
+		namePattern := regexp.MustCompile(`\b` + name + `\b`)
+		containsName := namePattern.MatchString(greeting)
+		if !containsName {
+			t.Fatalf(`Hellos([]string{"Gladys, Joe, Jack"}) break for name %q, value %v`, name, greeting)
+		}
+	}
+	if err != nil {
+		t.Fatalf(`For Hellos([]string{"Gladys, Joe, Jack"}) err should be nil`)
+	}
+}
+
+func TestHellosEmptyList(t *testing.T) {
+	// given
+	names := []string{}
+	// when
+	greetingsMap, err := Hellos(names)
+	// then
+	if greetingsMap != nil || err == nil {
+		t.Fatalf(`Hellos([]string{}) = %q, %v, want nil, error`, greetingsMap, err)
+	}
+}
+
+func TestHellosEmptyNameInList(t *testing.T) {
+	// given
+	names := []string{"Gladys", "", "Jack"}
+	// when
+	greetingsMap, err := Hellos(names)
+	// then
+	if greetingsMap != nil || err == nil {
+		t.Fatalf(`Hellos([]string{"Gladys", "", "Jack"}) = %q, %v, want nil, error`, greetingsMap, err)
 	}
 }
